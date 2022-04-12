88215f7ab3ddee55147ded8220a0592252254bba
nykolyshyn danylo <danylo.nykolyshyn@gmail.com>
2022-04-12 17:44:22 +0300

    StreamTask classes task

--- src/Tasks/StreamTask.java
+++ src/Tasks/StreamTask.java
@@ -8,7 +8,7 @@ public class StreamTask {
                 List.of(new Person("women","anna",20)
                 , new Person("man","ivan",12)
                 , new Person("man","petro",20)
-                ,new Person("man","vitaliy",90)
+                , new Person("man","vitaliy",90)
                 , new Person("women","olga",90));
         Builder peopleBuilder = new RealizePersonBuilder();
 

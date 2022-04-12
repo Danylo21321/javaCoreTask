f9357ff9b7fc1e316109c18c3f6ee736a91b7a83
nykolyshyn danylo <danylo.nykolyshyn@gmail.com>
2022-04-12 10:45:17 +0300

    Initial commit

---
+++ src/Tasks/InnerClass.java
@@ -0,0 +1,17 @@
+package Tasks;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class InnerClass {
+    public static void main(String[] args) {
+
+    }
+}
+@Data
+@AllArgsConstructor
+class Product{
+    private String nameModel;
+    private int sum;
+    private int serialNumber;
+}
